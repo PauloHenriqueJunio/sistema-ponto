a08c414b095d0960b8c44309e12e8bf0b364e877
Paulo Henrique <paulohenriquejunio@hotmail.com>
2026-01-29 14:52:31 -0300

    testing cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -180,10 +180,4 @@ app.listen(PORT, () => {
 });
 
 
-app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'https://sistema-ponto-liart.vercel.app'
-  ],
-  credentials: true
-}));
+app.use(cors());
